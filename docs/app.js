407f7797b87bef6096eabd4a9d146a9667f94c8a
Marius1410 <151372850+Marius1410@users.noreply.github.com>
2024-04-08 12:16:50 +0300

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -9,7 +9,6 @@ const remainingTriesDisplay = document.getElementById("remaining-tries");
 const letterInput = document.getElementById("letter-input");
 const guessBtn = document.getElementById("guess-btn");
 const resetBtn = document.getElementById("reset-btn");
-
 function initializeGame() {
     selectedWord = words[Math.floor(Math.random() * words.length)].toUpperCase();
     guessedWord = Array(selectedWord.length).fill('_');
@@ -17,12 +16,10 @@ function initializeGame() {
     remainingTries = 6;
     updateDisplay();
 }
-
 function updateDisplay() {
     wordDisplay.textContent = guessedWord.join(' ');
     wrongLettersDisplay.textContent = wrongLetters.join(', ');
     remainingTriesDisplay.textContent = remainingTries;
-
     if (guessedWord.join('') === selectedWord) {
         alert('Felicitări! Ai câștigat!');
         initializeGame();
@@ -31,7 +28,6 @@ function updateDisplay() {
         initializeGame();
     }
 }
-
 function guessLetter() {
     const letter = letterInput.value.toUpperCase();
     if (!letter.match(/[A-Z]/)) {
@@ -52,23 +48,19 @@ function guessLetter() {
         wrongLetters.push(letter);
         remainingTries--;
     }
-    letterInput.value = ""; // Golește input-ul
+    letterInput.value = "";
     updateDisplay();
 }
-
 function resetGame() {
     letterInput.value = "";
     initializeGame();
 }
-
 initializeGame();
 guessBtn.addEventListener('click', guessLetter);
 resetBtn.addEventListener('click', resetGame);
-
 letterInput.addEventListener('keypress', function(event) {
     if (event.key === "Enter") {
         guessLetter();
     }
 });
-
-letterInput.setAttribute("maxlength", "30");
+letterInput.setAttribute("maxlength", "10");
